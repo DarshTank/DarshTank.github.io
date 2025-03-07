368cb85bbb86b8d05dac04197c573c37fa8cd7b7
DarshTank <darshtank05@gmail.com>
2025-03-08 04:08:30 +0530

    General Updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Mail, Github, Linkedin, Menu, X } from "lucide-react";
 import { motion } from "framer-motion";
 import { useInView } from "react-intersection-observer";
